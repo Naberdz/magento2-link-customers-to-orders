b15be01a7886d92bbcb41a53a4e560915a746d81
Naberd <naberd@wemessage.nl>
2024-03-28 14:39:48 +0100

    converted code to spaces

--- Observer/Frontend/Customer/RegisterSuccess.php
+++ Observer/Frontend/Customer/RegisterSuccess.php
@@ -6,37 +6,37 @@ namespace Wemessage\LinkCustomers\Observer\Frontend\Customer;
 
 class RegisterSuccess implements \Magento\Framework\Event\ObserverInterface
 {
-	private $orderCollection;
-	
-	public function __construct(
-		\Magento\Sales\Model\ResourceModel\Order\CollectionFactory $orderCollection
-	){
-		$this->orderCollection = $orderCollection;
-	}
+    private $orderCollection;
+    
+    public function __construct(
+        \Magento\Sales\Model\ResourceModel\Order\CollectionFactory $orderCollection
+    ){
+        $this->orderCollection = $orderCollection;
+    }
 
     public function execute(
         \Magento\Framework\Event\Observer $observer
     ) {
-		$customer = $observer->getEvent()->getCustomer();
-		if($customer->getId()){
-			$customerOrder = $this->orderCollection->create()->addAttributeToFilter('customer_email', $customer->getEmail());
-			foreach($customerOrder as $order){
-				if($order->getCustomerIsGuest()) $this->linkOrderToCustomer($order, $customer);
-			}
-		}
+        $customer = $observer->getEvent()->getCustomer();
+        if($customer->getId()){
+            $customerOrder = $this->orderCollection->create()->addAttributeToFilter('customer_email', $customer->getEmail());
+            foreach($customerOrder as $order){
+                if($order->getCustomerIsGuest()) $this->linkOrderToCustomer($order, $customer);
+            }
+        }
     }
-	
-	private function linkOrderToCustomer($order, $customer){
-		
-		if ($customer->getId()) {
-			try {
-				$order->setCustomerId($customer->getId());
-				$order->setCustomerIsGuest(0);
-				$order->save();
-			} catch(\Exception $e){
-				
-			}
-		}
-	}
+    
+    private function linkOrderToCustomer($order, $customer){
+        
+        if ($customer->getId()) {
+            try {
+                $order->setCustomerId($customer->getId());
+                $order->setCustomerIsGuest(0);
+                $order->save();
+            } catch(\Exception $e){
+                
+            }
+        }
+    }
 }
 
--- Observer/Frontend/Sales/OrderPlaceAfter.php
+++ Observer/Frontend/Sales/OrderPlaceAfter.php
@@ -6,13 +6,13 @@ namespace Wemessage\LinkCustomers\Observer\Frontend\Sales;
 
 class OrderPlaceAfter implements \Magento\Framework\Event\ObserverInterface
 {
-	private $customerRepository;
-	
-	public function __construct(
-		\Magento\Customer\Model\ResourceModel\CustomerRepository $customerRepository
-	){
-		$this->customerRepository = $customerRepository;
-	}
+    private $customerRepository;
+    
+    public function __construct(
+        \Magento\Customer\Model\ResourceModel\CustomerRepository $customerRepository
+    ){
+        $this->customerRepository = $customerRepository;
+    }
 
     public function execute(
         \Magento\Framework\Event\Observer $observer
@@ -21,18 +21,18 @@ class OrderPlaceAfter implements \Magento\Framework\Event\ObserverInterface
 
         if (!$order->getCustomerId()) {
             $email = $order->getCustomerEmail();
-			try {
-				$customer = $this->customerRepository->get($email, $order->getStore()->getWebsiteId());
-				
-				if ($customer->getId()) {
-					$order->setCustomerId($customer->getId());
-					$order->setCustomerIsGuest(0);
-					$order->save();
-				}
-			} catch (\Exception $e) {
-				
-			}
-		}
+            try {
+                $customer = $this->customerRepository->get($email, $order->getStore()->getWebsiteId());
+                
+                if ($customer->getId()) {
+                    $order->setCustomerId($customer->getId());
+                    $order->setCustomerIsGuest(0);
+                    $order->save();
+                }
+            } catch (\Exception $e) {
+                
+            }
+        }
     }
 }
 
